1dbb3c30804e3ffe8b72629455608e54628b50c7
Dan-Mach <danreech83@gmail.com>
2025-02-26 20:02:26 +0300

    2d array

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
 fn main (){
-    return 
+    let sized: MySuperSliceable<[u8; 8]> = MySuperSliceable {
+        info: 17,
+        data:[0;8],
+    };
+
+    let dynamic = &sized;
+
+    //prints: "17 [0, 0, 0, 0, 0, 0, 0, 0]"
+    println!("{} {:?}", dynamic.info, dynamic.data);
+}
+
+struct MySuperSliceable<T: ?Sized> {
+    info:u32,
+    data: T,
 }
